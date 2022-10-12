6c1b235d430101de15c8126c559654232c4ac44b
unknown <jabrijuhinin93@gmail.com>
2022-10-12 21:13:26 +0800

    Finish Section 7(Filtering), Ep. 43

--- routes/web.php
+++ routes/web.php
@@ -1,53 +1,13 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Post;
 use App\Models\User;
 use App\Models\Category;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Facade;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
-// below is the initial code. the code causes repetitive request. so if we have 50 blog post, it will fetch 50 times in the SQL to perform the task. 
-// return view('posts', [
-//     'posts' => Post::all
-// ]);
-
-// Route::get('/', function () {
-//     return view('components.layout');
-// });
-
-
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
-
-Route::get('categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'posts' => $category->posts,
-        'currentCategory' => $category,
-        'categories' => Category::all()
-    ]);
-})->name('category');
-
-Route::get('authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'posts' => $author->posts,
-        'categories' => Category::all()
-    ]);
-}); 
 
 // Using array_map
 // $posts = array_map(function ($file) {
